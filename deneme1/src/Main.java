2102c42360fe4df1c8d7fbe00d8fdb7f1b9882f4
ahmet rasit keser <121893563+ahmetrkeser@users.noreply.github.com>
2023-02-14 22:08:43 +0300

    Add files via upload

---
+++ deneme1/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double urun ;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Ürün fiyatını giriniz: ");
+        urun = inp.nextInt();
+
+        double kdv = (urun)*0.18;
+        System.out.println("ürün kdv değeri : " + kdv);
+        double kdvli = (urun+kdv);
+        System.out.println("ürün kdv'li değeri : " + kdvli);
+
+    }
+}
